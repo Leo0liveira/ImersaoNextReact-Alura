b141579cd8896a3cf323e9cf25725c0afe77dd4a
Leo0liveira <leoufms@gmail.com>
2021-01-26 12:11:06 -0400

    fix

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
     return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
-        <title>SALVE</title>
+        <title>Chess Quiz - Alura</title>
       </Head>
       <QuizContainer>
         <QuizLogo />
