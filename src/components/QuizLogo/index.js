d6588af60d7fda6eb64b25a330ca140f5d72d7c5
Leo0liveira <leoufms@gmail.com>
2021-01-26 19:36:36 -0400

    fix

--- src/components/QuizLogo/index.js
+++ src/components/QuizLogo/index.js
@@ -1,13 +1,10 @@
 import styled from 'styled-components';
 
 const QuizLogo = styled.img`
-width:70%;
+width:58%;
 height: 50%;
 margin: auto;
 display:block;
-@media screen and (max-width: 500px){
-  margin: 0%;
-}
 `;
 
 export default QuizLogo;
